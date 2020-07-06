ba70649aac82ae5e697df1467f9dbea4986b6b37
membraned <aidanheck@gmail.com>
2020-07-06 08:17:03 -0400

    Added a loop that lists each Pokémon and their height.
    This loop also identifies Pokémon that are over 5ft tall.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 var pokemonList = [
-    { name: 'Chatot', height: 2, types: ['flying', 'normal'] },
-    { name: 'Aromatisse', height: 3, types: 'fairy' },
-    { name: 'Scolipede', height: 8, types: ['bug', 'poison'] },
-]
+    {
+        name: 'Chatot',
+        height: 2,
+        types: ['flying', 'normal']
+    },
+    {
+        name: 'Aromatisse',
+        height: 3,
+        types: ['fairy']
+    },
+    {
+        name: 'Scolipede',
+        height: 8,
+        types: ['bug', 'poison']
+    },
+];
+for (var i = 0; i < pokemonList.length; i++) {
+    document.write('<h6>' + pokemonList[i].name + '</h6>' + ' Height: ' + pokemonList[i].height)
+    if (pokemonList[i].height > 5) {
+        document.write(' - Wow, that\'s big!')
+    }
+}
