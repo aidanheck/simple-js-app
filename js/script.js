e2dcb1a97a326be242f103a1012116015b84a2c4
membraned <aidanheck@gmail.com>
2020-07-23 08:33:06 -0400

    Created addListItem function and added CSS styling to basic pokédex.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function printArrayDetails(list) {
 printArrayDetails(pokemonList);
 }*/
 
-
 /*
 forEach function for Task 1.4
 pokemonList.forEach(function (pokemon) {
@@ -44,61 +43,107 @@ pokemonList.forEach(function (pokemon) {
 */
 //IIFE Function
 var pokemonRepository = (function () {
+  var pokemonList = [
+    {
+      name: "Chatot",
+      height: 2,
+      types: ["flying", " normal"],
+    },
+    {
+      name: "Aromatisse",
+      height: 3,
+      types: ["fairy"],
+    },
+    {
+      name: "Scolipede",
+      height: 8,
+      types: ["bug", " poison"],
+    },
+  ];
 
-    var pokemonList = [
-        {
-            name: 'Chatot', height: 2, types: ['flying', ' normal']
-        },
-        {
-            name: 'Aromatisse', height: 3, types: ['fairy']
-        },
-        {
-            name: 'Scolipede', height: 8, types: ['bug', ' poison']
-        },];
-
-    function getAll() {
-        return pokemonList;
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
     }
+  }
 
-    function add(pokemon) {
-        if (
-            typeof pokemon === "object" &&
-            "name" in pokemon &&
-            "height" in pokemon &&
-            "types" in pokemon
-        ) {
-            pokemonList.push(pokemon);
-        }
-    }
-    function getAll() {
-        return pokemonList;
-    }
-    return {
-        add: add,
-        getAll: getAll
-    };
+  function getAll() {
+    return pokemonList;
+  }
 
+  function addListItem(pokemon) {
+    var pokemonList = document.querySelector(".pokemon-list");
+    var listItem = document.createElement("li");
+    var button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-style");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu', height: 1, types: ['electric'] });
+pokemonRepository.add({ name: "Pikachu", height: 1, types: ["electric"] });
+// pokemonRepository.add({ height: 4, types: ['flying'] });
 console.log(pokemonRepository.getAll());
 
 //IIFE forEach function
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-    var size;
+  var size;
+  if (pokemon.height < 3) {
+    size = "it's a small pokemon!";
+  } else if (pokemon.height >= 3 && pokemon.height < 5) {
+    size = "it's a medium pokemon!";
+  } else {
+    size = "it's a large pokemon!";
+  }
 
-    if (pokemon.height < 3) {
-        size = "it's a small pokemon!";
+  var result;
+  pokemon.types.forEach(function (typesItem) {
+    if (typesItem == "bug") {
+      result = '<span style="color:green;"> ';
+    } else if (typesItem == "fairy") {
+      result = '<span style="color:red;"> ';
+    } else if (typesItem == "flying") {
+      result = '<span style="color:yellow;"> ';
+    } else if (typesItem == "poison") {
+      result = '<span style="color:rgb(106, 42, 106);"> ';
+    } else if (typesItem == "electric") {
+      result = '<span style="color:orange;"> ';
     }
-    else if (pokemon.height >= 3 && pokemon.height < 5) {
-        size = "it's a medium pokemon!";
-    }
-    else {
-        size = "it's a large pokemon!";
-    };
-}
-)
+  });
 
+  // document.write(
+  //   result +
+  //     "name: " +
+  //     pokemon.name +
+  //     " (height: " +
+  //     pokemon.height +
+  //     " ft)" +
+  //     " " +
+  //     size +
+  //     "<br>" +
+  //     "types: " +
+  //     pokemon.types +
+  //     "<br>"
+  // );
 
+  pokemonRepository.addListItem(pokemon);
+});
